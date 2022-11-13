b4edc9a3aceb65428e9d190da7342e305e9e2181
YuYang-Dev <79308509+YuYang-Dev@users.noreply.github.com>
2022-11-13 10:01:53 +0800

    Update RandomMovement.cs

--- RandomMovement.cs
+++ RandomMovement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI; //important
 
-//if you use this code you are contractually obligated to like the YT video
 public class RandomMovement : MonoBehaviour //don't forget to change the script name if you haven't
 {
     public NavMeshAgent agent;
